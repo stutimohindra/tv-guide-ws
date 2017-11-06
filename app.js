87434326bfdb4f2cdf64e6008b7e346d54373641
stuti@lizard-apps.com <stuti@lizard-apps.com>
2017-11-06 16:55:10 +0800

    adding allow orign

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.text());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
